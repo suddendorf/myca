926629bf11cc6ea246fc83633b9c5de9c40b845f
Rainer Suddendorf <suddendorf@sddb.de>
2021-04-03 10:51:31 +0200

    mm

--- src/app/chess/chess.component.ts
+++ src/app/chess/chess.component.ts
@@ -23,33 +23,74 @@ export class ChessComponent implements OnInit {
 
   zuege: string[];
   status: string;
-  cellHeight:number;
+  cellHeight: number;
+  myColor: string;
+  canMove = true;
+  interval: any;
+  rotate=false;
+  vBrett: string[];
   constructor(private service: DataService) { }
 
   ngOnInit(): void {
-   let w = Math.min(window.innerWidth,window.innerHeight);
-    this.cellHeight = (w*0.9)  / 8;
-    console.log(w+">"+window.innerHeight+":"+window.innerWidth);
-    this.service.getJSON('brett')
+    let w = Math.min(window.innerWidth, window.innerHeight);
+    this.cellHeight = (w * 0.5) / 8;
+    console.log(w + ">" + window.innerHeight + ":" + window.innerWidth);
+    this.interval = setInterval(() => {
+      this.read();
+    }, 1000 * 1);
+  }
+
+  read() {
+    console.log('read');
+    this.service.getJSON('color')
       .subscribe(
-        ret => this.brett = ret,
-        error => console.log(error));
-    this.service.getJSON('zuege')
-      .subscribe(
-        ret => {this.zuege = ret; this.scrollToBottom('zuege');}
-        ,
+        color => this.readDetails(color),
         error => console.log(error));
   }
- scrollToBottom (id:string) {
+  readDetails(color: string) {
+    console.log('my'+this.myColor+' color:'+color);
+    if (color == 'white') {
+      this.status = 'black';
+    } else {
+      this.status = 'white';
+    }
+
+    if (this.myColor != color) {
+      if ( this.myColor){
+        clearInterval(this.interval);
+      }
+      this.canMove = true;
+      this.service.getJSON('brett')
+        .subscribe(
+          ret => this.setBrett(ret),
+          error => console.log(error));
+      this.service.getJSON('zuege')
+        .subscribe(
+          ret => { this.zuege = ret; this.scrollToBottom('zuege'); }
+          ,
+          error => console.log(error));
+    }
+  }
+  setBrett(brett: string []): void {
+    this.brett = brett;
+    this.vBrett =brett;
+    if (this.rotate){
+      for ( let i = 0;i<brett.length;i++){
+    //    this.vBrett[63-i]=this.brett[i];
+      }
+    }
+  }
+
+  scrollToBottom(id: string) {
     var div = document.getElementById(id);
     div.scrollTop = div.scrollHeight - div.clientHeight;
- }
+  }
   onResize(event: any) {
-    this.cellHeight = window.innerWidth / 8;
+    this.cellHeight = window.innerWidth*0.5 / 8;
 
   }
-  getHeight(){
-    return this.cellHeight*8+'px';
+  getHeight() {
+    return this.cellHeight * 8 + 'px';
   }
   reset(): void {
     this.brett = ChessComponent.brettStart;
@@ -62,10 +103,18 @@ export class ChessComponent implements OnInit {
       .subscribe(
         ret => console.log(ret),
         error => console.log(error));
+
+    this.service.putJSON('color', JSON.stringify('none'))
+      .subscribe(
+        ret => console.log(ret),
+        error => console.log(error));
     this.zug = null;
     this.kFrom = null;
     this.kTo = null;
-    this.status = 'White';
+    this.myColor = null;
+    this.canMove = true;
+    this.rotate = false;
+    this.vBrett = this.brett;
   }
   move(): void {
     let from = this.kFrom;
@@ -73,27 +122,91 @@ export class ChessComponent implements OnInit {
     let to = this.kTo;//ChessComponent.getPos(this.zug.substring(3));
     if (to == null) return;
     console.log("to:" + to);
-    this.brett[to] = this.brett[from];
-    this.brett[from] = "";
+    this.moveWithRule(from,to);
     this.service.putJSON('brett', JSON.stringify(this.brett))
       .subscribe(
         ret => this.addZug(this.zug),
         error => console.log(error));
-
     this.kFrom = this.kTo = null;
+    this.canMove = false;
+    this.interval = setInterval(() => {
+      this.read();
+    }, 1000 * 1);
+
+  }
+  moveWithRule(from:number,to:number) {
+    const figur= this.brett[from];
+    const row = Math.floor(to/8);
+    //rochade
+    if ( figur.substring(0,2) == 'wK' && from ==60){
+      this.brett[to] = this.brett[from];
+      this.brett[from] = "";
+      if ( to == 62){
+        this.brett[61] = 'wT2';
+        this.brett[63] = "";
+        return;
+      }
+      if ( to == 58){
+        this.brett[59] = 'wT1';
+        this.brett[56] = "";
+        return;
+      }
+    }
+
+    if ( figur.substring(0,2) == 'sK' && from ==4){
+      this.brett[to] = this.brett[from];
+      this.brett[from] = "";
+      if ( to == 6){
+        this.brett[5] = 'sT2';
+        this.brett[7] = "";
+        return;
+      }
+      if ( to == 2){
+        this.brett[3] = 'sT1';
+        this.brett[0] = "";
+        return;
+      }
+    }
+    //Bauerntausch
+    if ( figur.substring(0,2) == 'wB'){
+      if ( row==0){
+        this.brett[to] = 'wD';
+        this.brett[from] = "";
+        return;
+      }
+    }
+    if ( figur.substring(0,2) == 'sB'){
+      if ( row==7){
+        this.brett[to] = 'sD';
+        this.brett[from] = "";
+        return;
+      }
+    }
+    // Standard
+    this.brett[to] = this.brett[from];
+    this.brett[from] = "";
+
   }
   addZug(zug: string): void {
+    if (this.zuege == null) {
+      this.zuege = [];
+    }
+    console.log(this.myColor);
+    if (this.zuege.length==0  ) {
+      this.myColor = 'white';
+    } else if (this.zuege.length == 1) {
+      this.myColor = 'black';
+    }
     this.zuege.push(this.zug);
 
-    const z  = Object.assign([], this.zuege);
-    this.zuege=z;
+    const z = Object.assign([], this.zuege);
+    this.zuege = z;
     this.scrollToBottom('zuege');
-    if (this.zuege.length %2 ==1){
-      this.status='schwarz';
-    }else{
-      this.status = 'weiß';
-    }
     this.service.putJSON('zuege', JSON.stringify(this.zuege))
+      .subscribe(
+        ret => console.log(ret),
+        error => console.log(error));
+    this.service.putJSON('color', JSON.stringify(this.myColor))
       .subscribe(
         ret => console.log(ret),
         error => console.log(error));
@@ -128,7 +241,7 @@ export class ChessComponent implements OnInit {
       this.kTo = null;
       return;
     }
-    if (this.kFrom==null) {
+    if (this.kFrom == null) {
       this.kFrom = i;
     } else {
       this.kTo = i;
@@ -169,5 +282,8 @@ export class ChessComponent implements OnInit {
   kFrom: number;
   kTo: number;
 
-
+  toggleRotate(){
+    this.rotate = !this.rotate;
+    this.setBrett(this.brett);
+  }
 }
